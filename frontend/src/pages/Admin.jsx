0118f8d5174df97a8249b42eff24be7f0c967a51
bhavya162 <bhavyasharma0558@gmail.com>
2026-03-04 17:10:53 +0530

    fix axios import

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import API from "../api";
 import "./Admin.css";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 
 function Admin() {
